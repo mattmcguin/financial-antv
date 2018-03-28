230e45fc21550e31ac64fb0fd21e1e463e02273c
Matt McGuiness <mattjmcguiness@gmail.com>
2018-03-28 16:39:49 -0400

    Remove Legend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import axios from "axios";
-import { Chart, Geom, Axis, Tooltip, Legend } from "bizcharts";
+import { Chart, Geom, Axis, Tooltip } from "bizcharts";
 
 const AppContainer = styled.div`
   display: flex;
